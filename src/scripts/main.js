1ddc158a8e3250bc99b37f5e133f6c4344a95e5c
Artem Maksimikhin <uanov86@gmail.com>
2021-03-23 13:03:43 +0200

    'solution'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.innerHTML = wordInHtml(input.name);
+  input.placeholder = wordInHtml(input.name);
+  label.htmlFor = input.placeholder;
+  input.parentElement.append(label);
+}
+
+function wordInHtml(string) {
+  return ((string[0].toUpperCase() + string.slice(1))
+    .replace(/([A-Z])/g, ' $1')).slice(1);
+}
